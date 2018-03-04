7dc740d5f1be458802948ac5718aa1662930380f
Sridhar Ratnakumar <srid@srid.ca>
2018-03-03 19:30:16 -0500

    Add beam dependency

--- default.nix
+++ default.nix
@@ -23,4 +23,18 @@
     ghc = ["common" "backend" "frontend"];
     ghcjs = ["common" "frontend"];
   };
+
+  overrides = self: super: let
+    beam = pkgs.fetchFromGitHub {
+      owner = "tathougies";
+      repo = "beam";
+      rev = "1673f0762f9beca04923518bca1ba5c519fd56a3";
+      sha256 = "0l1zs64n736gkrz3bs0ws6lq4kjig32przdmnjigk40rs3mly32k";
+    };
+  in
+  {
+    beam-core = self.callCabal2nix "beam-core" "${beam}/beam-core" {};
+    beam-migrate = self.callCabal2nix "beam-migrate" "${beam}/beam-migrate" {};
+    beam-sqlite = self.callCabal2nix "beam-sqlite" "${beam}/beam-sqlite" {};
+  };
 })
