bea4d6b7b4323fcdc11f4103bedff73c6cd26b9e
Sridhar Ratnakumar <srid@srid.ca>
2018-09-08 20:34:46 -0400

    Add semantic UI

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 module Frontend where
 
@@ -8,14 +11,27 @@ import Reflex.Dom.Core
 import Obelisk.Frontend
 import Obelisk.Route.Frontend
 
+import Static
+
 import Common.Route
 
 frontend :: Frontend (R Route)
 frontend = Frontend
-  { _frontend_head = subRoute_ $ \_r -> do
-      el "title" $ text "Taut"
+  { _frontend_head = do
+      elAttr "base" ("href" =: "/") blank
+      elAttr "meta" ("name" =: "viewport" <> "content" =: "width=device-width, initial-scale=1") blank
+      el "title" $ do
+        r <- askRoute
+        dynText $ ffor r $ \case
+          (_ :: R Route) -> "Taut" -- Placeholder for title changing logic
+      elAttr "link" ("rel" =: "stylesheet" <> "type" =: "text/css" <> "href" =: static @"semantic.min.css") blank
   , _frontend_body = subRoute_ $ \_r -> do
-      el "h1" $ text "Taut"
-      el "p" $ text "Work in progress"
+      divClass "ui container" $ do
+        divClass "ui top attached inverted header" $ do
+          text "Taut"
+        divClass "ui attached segment" $ do
+          el "p" $ text "We'll show your Slack archive here. Hold tight!"
+        divClass "ui bottom attached secondary segment" $ do
+          el "p" $ text "This is a work in progress"
   , _frontend_notFoundRoute = \_ -> Route_Home :/ () -- TODO: not used i think
   }
