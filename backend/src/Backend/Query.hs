ccf9880ee40d03dbf6e043a2f3b0821f96c3b184
Sridhar Ratnakumar <srid@srid.ca>
2019-05-08 21:47:48 -0400

    Getting close to a workeable query parser

--- backend/src/Backend/Query.hs
+++ backend/src/Backend/Query.hs
@@ -1,79 +1,74 @@
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Backend.Query where
 
+import Data.Foldable (foldr1)
+import Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NEL
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Time.Calendar
-import Data.Foldable (foldr1)
 import Data.Time.Clock
-import qualified Data.List.NonEmpty as NEL
 import Data.Void
-import Data.List.NonEmpty (NonEmpty (..))
 
 import Database.Beam
 import Database.Beam.Backend.SQL
 import Text.Megaparsec
 import Text.Megaparsec.Char
-import qualified Text.Megaparsec.Char.Lexer as L
+-- import qualified Text.Megaparsec.Char.Lexer as L
 
 import Common.Slack.Types
 
 import Backend.Import (SlackDb (..))
 
-data SearchQuery
-  = SearchQuery_Like Text
-  | SearchQuery_Or (NonEmpty SearchQuery)
-  | SearchQuery_From Text
-  | SearchQuery_To Text
-  deriving (Show)
+data SearchModifier
+  = SearchModifier_From Text  -- ^ Sent by person
+  | SearchModifier_In Text  -- ^ In channel
+  | SearchModifier_Before Day
+  | SearchModifier_After Day
+  -- | SearchModifier_During (Either Day (Either Int Int)  -- On day, month or year.
+  deriving Show
+
+newtype SearchKeyword = SearchKeyword Text
+  deriving Show
 
 type Parser = Parsec Void Text
 
-someTerm :: Parser Text
-someTerm = fmap T.pack $ some alphaNumChar
+searchModifier :: Parser SearchModifier
+searchModifier =
+      try (SearchModifier_From <$> attribute "from")
+  <|> try (SearchModifier_In <$> attribute "in")
 
-searchQuery :: Parser SearchQuery
-searchQuery = fmap combineOr p
-  where
-    p =
-        (SearchQuery_From <$> (L.symbol space "from:" >> someTerm))
-      <|> (SearchQuery_To <$> (L.symbol space "to:" >> someTerm))
-      <|> (SearchQuery_Like . T.pack <$> some alphaNumChar) -- (sepEndBy1 printChar space1))
-    combineOr = id
-  -- <|> (SearchQuery_Or . fromJust . NEL.nonEmpty <$> sepEndBy1 searchQuery space1)
+searchKeyword :: Parser SearchKeyword
+searchKeyword = SearchKeyword . T.pack <$> some alphaNumChar  -- TODO: support quoted strings
+
+attribute :: Text -> Parser Text
+attribute name = do
+  _ <- string name
+  _ <- string ":"
+  T.pack <$> some alphaNumChar
 
 demo :: IO ()
-demo = parseTest searchQuery "Hello world"
+demo = parseTest (sepBy (Left <$> try searchModifier <|> Right <$> searchKeyword) space1) "from:srid hello world  in:general more  query"
 
-data C = C
+data MessageFilters = MessageFilters
   { _c_terms :: [Text]
-  , _c_after :: Maybe UTCTime
-  , _c_before :: Maybe UTCTime
-  , _c_during :: Maybe (Either UTCTime UTCTime)
-  , _c_from :: Maybe [Text]
-  , _c_to :: Maybe [Text]
+  , _c_from :: [Text]
+  , _c_after :: Maybe Day
+  , _c_before :: Maybe Day
+  -- , _c_during :: [Either UTCTime UTCTime]
   }
 
--- TODO: Provide example queries for each constructor and display that in home
--- page. (Store queries in config file)
 data Query
   = Query_Day Day
   | Query_Like Text
   | Query_And (NonEmpty Text)
-  -- | Query_Exact Text
-  -- | Query_Or Query Query
-  -- | Query_And Query Query
-  -- | Query_After UTCTime
-  -- | Query_Between UTCTime
-  -- | Query_During (Either Month Year)
-  -- | Query_From SlackUser
 
 parseQuery :: Text -> Query
 parseQuery query = case NEL.nonEmpty (T.words query) of
