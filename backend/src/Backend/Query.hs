c1b24a2d3a3642ab0ab95963a4f266dd2d57ba77
Sridhar Ratnakumar <srid@srid.ca>
2019-05-10 18:23:19 -0400

    WIP: during: query

--- backend/src/Backend/Query.hs
+++ backend/src/Backend/Query.hs
@@ -10,11 +10,15 @@
 module Backend.Query where
 
 import Control.Lens hiding ((<.))
+import Control.Monad
 import qualified Data.List.NonEmpty as NEL
 import Data.Maybe
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Time.Calendar
+import Data.Time.Clock
 import Data.Void
+import Text.Read (readMaybe)
 
 import Database.Beam
 import Database.Beam.Backend.SQL
@@ -26,16 +30,21 @@ import Common.Slack.Types
 
 import Backend.Import (SlackDb (..))
 
+-- Learnings and observations:
+-- * Megaparsec tutorial is long. Summarize key points to know?
+--   * For someone that already is basically familiar with parser combinators
+-- * Testing using hspec-megaparsec
+-- * Importance of `try`
 
 data SearchModifier
   = SearchModifier_From Text  -- ^ Sent by person
   | SearchModifier_In Text  -- ^ In channel
+  | SearchModifier_During Day
   -- | SearchModifier_Before Day
   -- | SearchModifier_After Day
   | SearchModifier_HasPin
   -- | HasLink
   -- | HasEmoji EmojiCode
-  -- | SearchModifier_During (Either Day (Either Int Int)  -- On day, month or year.
   deriving (Show, Generic)
 
 newtype SearchKeyword = SearchKeyword { unSearchKeyword :: Text }
@@ -47,6 +56,20 @@ searchModifier :: Parser SearchModifier
 searchModifier =
       try (SearchModifier_From <$> attribute "from")
   <|> try (SearchModifier_In <$> attribute "in")
+  <|>     (SearchModifier_During <$> do
+              -- TODO: parse this properly!
+              -- FIXME: this is not throwing properly on invalid days
+              let parseNum :: Read a => String -> Parser a
+                  parseNum e = maybe (fail e :: Parser a) pure =<< fmap readMaybe (some digitChar)
+              void $ try $ string "during:"
+              year :: Integer <- parseNum "Expected year"
+              _ <- string "-"
+              month :: Int <- parseNum "Expected month"
+              _ <- string "-"
+              day :: Int <- parseNum "Expected day"
+              -- TODO: get rid of fromJust
+              maybe (fail "Invalid day") pure $ fromGregorianValid year month day
+          )
   <|> try (SearchModifier_HasPin <$ string "has:pin")
 
 searchKeyword :: Parser SearchKeyword
@@ -58,42 +81,37 @@ attribute name = do
   _ <- string ":"
   T.pack <$> some alphaNumChar
 
+-- TODO: Add unit tests for this function.
 parseSearchQuery :: Text -> Either (ParseError Char Void) [Either SearchModifier SearchKeyword]
 parseSearchQuery q = runParser p "<user-query>" q
   where
-    p = sepBy (Left <$> try searchModifier <|> Right <$> searchKeyword) space1
+    p = sepBy (Left <$> searchModifier <|> Right <$> searchKeyword) space1
+
 
 data MessageFilters = MessageFilters
   { _messageFilters_terms :: [SearchKeyword]
   , _messageFilters_from :: [Text]
   , _messageFilters_in :: [Text]
+  , _messageFilters_during :: [Day]
   , _messageFilters_hasPin :: Maybe ()
-  -- , _messageFilters_after :: Maybe Day
-  -- , _c_before :: Maybe Day
-  -- , _c_during :: [Either UTCTime UTCTime]
   }
   deriving (Show, Generic)
 
 makeLenses ''MessageFilters
 
 allMessages :: MessageFilters
-allMessages = MessageFilters [] [] [] Nothing
+allMessages = MessageFilters [] [] [] [] Nothing
 
 mkMessageFilters :: [Either SearchModifier SearchKeyword] -> MessageFilters
-mkMessageFilters = foldl f ini
+mkMessageFilters = foldl f allMessages
   where
-    ini = MessageFilters mempty mempty mempty Nothing
     f mf = \case
       Right kw -> mf & messageFilters_terms %~ (kw:)
       Left md -> case md of
         SearchModifier_From user -> mf & messageFilters_from %~ (user:)
         SearchModifier_In chan -> mf & messageFilters_in %~ (chan:)
+        SearchModifier_During day -> mf & messageFilters_during %~ (day:)
         SearchModifier_HasPin -> mf & messageFilters_hasPin ?~ ()
-
-demo :: IO ()
-demo = parseTest
-  (sepBy (Left <$> try searchModifier <|> Right <$> searchKeyword) space1)
-  "from:srid hello world  has:pin in:general more  query"
 
 messageFilters
   :: forall be s.
@@ -101,7 +119,7 @@ messageFilters
      , BeamSqlBackendIsString be Text
      , HasSqlValueSyntax (BeamSqlBackendValueSyntax be) Text
      , HasSqlEqualityCheck be Text
-     -- , HasSqlValueSyntax (BeamSqlBackendValueSyntax be) UTCTime
+     , HasSqlValueSyntax (BeamSqlBackendValueSyntax be) UTCTime
      )
   => MessageFilters
   -> Q be SlackDb s (MessageT (QExpr be s))
@@ -111,7 +129,13 @@ messageFilters mf = filter_ $ \msg -> foldl (&&.) (val_ True) $ catMaybes $
       msgContaining msg . unSearchKeyword <$> mf ^. messageFilters_terms
   , maybe Nothing (Just . foldl1 (||.)) $ NEL.nonEmpty $
       msgFrom msg <$> mf ^. messageFilters_from
+  , maybe Nothing (Just . foldl1 (||.)) $ NEL.nonEmpty $
+      msgDuring msg <$> mf ^. messageFilters_during
   ]
   where
     msgContaining msg q = _messageText msg `like_` val_ ("%" <> q <> "%")
     msgFrom msg (user :: Text) = _messageUserName msg ==. just_ (val_ user) -- FIXME: join with userName
+    msgDuring msg day = _messageTs msg >=. val_ fromTs &&. _messageTs msg <. val_ toTs
+      where
+        fromTs = UTCTime day 0
+        toTs = UTCTime (addDays 1 day) 0
